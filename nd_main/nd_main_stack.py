c63e75a07bcdbc4efe98139baef0b58155257082
Vincent Chan <vincent@doingcloudstuff.com>
2023-02-21 03:09:14 -0800

    refactor main stack

--- nd_main/nd_main_stack.py
+++ nd_main/nd_main_stack.py
@@ -41,95 +41,83 @@ _APIGW_FULL_PERMISSION_POLICY = "AmazonAPIGatewayAdministrator"
 
 
 class NdMainStack(Stack):
-    def __init__(
-        self,
-        scope: Construct,
-        construct_id: str,
-        prefix: str,
-        domain_name: str,
-        region_name: str,
-        **kwargs,
-    ) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def import_dynamodb_table(self, name: str) -> dynamodb.ITable:
+        return dynamodb.Table.from_table_name(self, name, f"{self.prefix}_{name}")
 
-        self.prefix = prefix
-        self.region_name = region_name
-
-        # secrets
-        _JWT_SECRET_NAME = "neurodeploy/mvp/jwt-secrets"
-        jwt_secret = sm.Secret.from_secret_name_v2(
-            self,
-            "jwt_secret",
-            secret_name=_JWT_SECRET_NAME,
-        )
-
-        # lambda layers
-        py_jwt_layer = lambda_.LayerVersion.from_layer_version_arn(
-            self,
-            "py_jwt_layer",
-            layer_version_arn="arn:aws:lambda:us-west-2:410585721938:layer:pyjwt:1",
-        )
-
-        # DNS
-        nd_zone = route53.HostedZone.from_lookup(
-            self,
-            "HostedZone",
-            domain_name=domain_name,
-        )
-        main_cert = acm.Certificate(
-            self,
-            "Certificate",
-            domain_name=f"*.{domain_name}",
-            validation=acm.CertificateValidation.from_dns(nd_zone),
-        )
-
-        # DynamoDB Table imports
+    def import_databases(self):
         self.users: dynamodb.Table = self.import_dynamodb_table("Users")
         self.apis: dynamodb.Table = self.import_dynamodb_table("APIs")
         self.tokens: dynamodb.Table = self.import_dynamodb_table("Tokens")
         self.models: dynamodb.Table = self.import_dynamodb_table("Models")
         self.usage_logs: dynamodb.Table = self.import_dynamodb_table("UsageLogs")
 
-        # S3 bucket imports
+    def import_buckets(self):
         self.models_bucket = s3.Bucket.from_bucket_name(
-            self, "models_bucket", bucket_name=f"{prefix}-models"
+            self, "models_bucket", bucket_name=f"{self.prefix}-models"
         )
 
-        # API Gateway
+    def import_secrets(self):
+        _JWT_SECRET_NAME = "neurodeploy/mvp/jwt-secrets"
+        self.jwt_secret = sm.Secret.from_secret_name_v2(
+            self,
+            "jwt_secret",
+            secret_name=_JWT_SECRET_NAME,
+        )
+
+    def import_lambda_layers(self):
+        self.py_jwt_layer = lambda_.LayerVersion.from_layer_version_arn(
+            self,
+            "py_jwt_layer",
+            layer_version_arn="arn:aws:lambda:us-west-2:410585721938:layer:pyjwt:1",
+        )
+
+    def import_hosted_zone(self) -> route53.IHostedZone:
+        zone = route53.HostedZone.from_lookup(
+            self,
+            "HostedZone",
+            domain_name=self.domain_name,
+        )
+        return zone
+
+    def create_api_gateway_and_lambdas(self):
         self.apigw_resources: Dict[str, apigw.Resource] = {}
         self.api = apigw.RestApi(
             self,
-            id=f"{prefix}_api",
+            id=f"{self.prefix}_api",
             domain_name=apigw.DomainNameOptions(
-                domain_name=f"api.{domain_name}", certificate=main_cert
+                domain_name=f"api.{self.domain_name}", certificate=self.main_cert
             ),
             endpoint_types=[apigw.EndpointType.REGIONAL],
         )
-        POST_signup = self.add(
+        self.POST_signup = self.add(
             "POST",
             "signup",
             tables=[(self.users, _READ_WRITE), (self.tokens, _READ_WRITE)],
             create_queue=True,
         )
-        POST_signup.lambda_function.add_environment("cert", main_cert.certificate_arn)
-        POST_signup.lambda_function.add_environment("domain_name", domain_name)
-        POST_signup.lambda_function.add_environment(
-            "hostd_zone_id", nd_zone.hosted_zone_id
+        self.POST_signup.lambda_function.add_environment(
+            "cert", self.main_cert.certificate_arn
         )
-        POST_signup.lambda_function.role.add_managed_policy(
+        self.POST_signup.lambda_function.add_environment(
+            "domain_name", self.domain_name
+        )
+        self.POST_signup.lambda_function.add_environment(
+            "hostd_zone_id", self.hosted_zone.hosted_zone_id
+        )
+        self.POST_signup.lambda_function.role.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name(_ACM_FULL_PERMISSION_POLICY)
         )
-        POST_signin = self.add(
+        self.POST_signin = self.add(
             "POST",
             "signin",
             tables=[
                 (self.users, _READ),
                 (self.tokens, _READ_WRITE),
             ],
-            secrets=[("jwt_secret", jwt_secret)],
-            layers=[py_jwt_layer],
+            secrets=[("jwt_secret", self.jwt_secret)],
+            layers=[self.py_jwt_layer],
         )
-        GET_access_token = self.add(
+        self.GET_access_token = self.add(
             "GET",
             "access_token",
             tables=[
@@ -139,10 +127,10 @@ class NdMainStack(Stack):
                 (self.models, _READ_WRITE),
                 (self.usage_logs, _READ_WRITE),
             ],
-            secrets=[("jwt_secret", jwt_secret)],
-            layers=[py_jwt_layer],
+            secrets=[("jwt_secret", self.jwt_secret)],
+            layers=[self.py_jwt_layer],
         )
-        POST_create_endpoint = self.add(
+        self.POST_create_endpoint = self.add(
             "POST",
             "create_endpoint",
             tables=[
@@ -152,10 +140,10 @@ class NdMainStack(Stack):
                 (self.models, _READ_WRITE),
                 (self.usage_logs, _READ_WRITE),
             ],
-            secrets=[("jwt_secret", jwt_secret)],
-            layers=[py_jwt_layer],
+            secrets=[("jwt_secret", self.jwt_secret)],
+            layers=[self.py_jwt_layer],
         )
-        POST_associate_ml_model = self.add(
+        self.POST_associate_ml_model = self.add(
             "POST",
             "associate_ml_model",
             tables=[
@@ -165,10 +153,10 @@ class NdMainStack(Stack):
                 (self.models, _READ_WRITE),
                 (self.usage_logs, _READ_WRITE),
             ],
-            secrets=[("jwt_secret", jwt_secret)],
-            layers=[py_jwt_layer],
+            secrets=[("jwt_secret", self.jwt_secret)],
+            layers=[self.py_jwt_layer],
         )
-        GET_ml_model_upload = self.add(
+        self.GET_ml_model_upload = self.add(
             "GET",
             "ml_model_upload",
             tables=[
@@ -179,51 +167,12 @@ class NdMainStack(Stack):
                 (self.usage_logs, _READ_WRITE),
             ],
             buckets=[(self.models_bucket, _READ_WRITE)],
-            secrets=[("jwt_secret", jwt_secret)],
-            layers=[py_jwt_layer],
+            secrets=[("jwt_secret", self.jwt_secret)],
+            layers=[self.py_jwt_layer],
         )
 
-        # Add record to route53 pointing "api" subdomain to api gateway
-        api_record = route53.ARecord(
-            self,
-            "ApiRecord",
-            record_name=f"api.{domain_name}",
-            zone=nd_zone,
-            target=route53.RecordTarget.from_alias(targets.ApiGateway(self.api)),
-        )
-
-        # new user lambda
-        new_user_lambda = lambda_.Function(
-            self,
-            "new_user_lambda",
-            function_name=f"{self.prefix}_new_user",
-            runtime=lambda_.Runtime.PYTHON_3_9,
-            code=lambda_.Code.from_asset("src"),
-            handler="new_user.handler",
-            timeout=Duration.seconds(300),
-            environment={"hosted_zone_id": nd_zone.hosted_zone_id},
-            layers=[],
-        )
-        POST_signup.queue.grant_consume_messages(new_user_lambda)
-        POST_signup.queue.grant_send_messages(new_user_lambda)
-        new_user_lambda.add_event_source(
-            event_sources.SqsEventSource(POST_signup.queue, batch_size=1)
-        )
-        permissions = [
-            _ACM_FULL_PERMISSION_POLICY,
-            _SQS_FULL_PERMISSION_POLICY,
-            _ROUTE_53_FULL_PERMISSION_POLICY,
-            _APIGW_FULL_PERMISSION_POLICY,
-        ]
-        for permission in permissions:
-            new_user_lambda.role.add_managed_policy(
-                iam.ManagedPolicy.from_aws_managed_policy_name(permission)
-            )
-        self.apis.grant_read_write_data(new_user_lambda)
-        new_user_lambda.add_environment(self.apis.table_name, self.apis.table_arn)
-
-        # delete user lambda
-        delete_user_lambda = lambda_.Function(
+    def create_delete_user_lambda(self):
+        self.delete_user_lambda = lambda_.Function(
             self,
             "delete_user_lambda",
             function_name=f"{self.prefix}_delete_user",
@@ -231,7 +180,7 @@ class NdMainStack(Stack):
             code=lambda_.Code.from_asset("src"),
             handler="delete_user.handler",
             timeout=Duration.seconds(300),
-            environment={"hosted_zone_id": nd_zone.hosted_zone_id},
+            environment={"hosted_zone_id": self.hosted_zone.hosted_zone_id},
             layers=[],
         )
         permissions = [
@@ -241,11 +190,51 @@ class NdMainStack(Stack):
             _APIGW_FULL_PERMISSION_POLICY,
         ]
         for permission in permissions:
-            delete_user_lambda.role.add_managed_policy(
+            self.delete_user_lambda.role.add_managed_policy(
                 iam.ManagedPolicy.from_aws_managed_policy_name(permission)
             )
-        self.apis.grant_read_write_data(delete_user_lambda)
-        delete_user_lambda.add_environment(self.apis.table_name, self.apis.table_arn)
+        self.apis.grant_read_write_data(self.delete_user_lambda)
+        self.delete_user_lambda.add_environment(
+            self.apis.table_name, self.apis.table_arn
+        )
+
+    def create_new_user_lambda(self):
+        self.new_user_lambda = lambda_.Function(
+            self,
+            "new_user_lambda",
+            function_name=f"{self.prefix}_new_user",
+            runtime=lambda_.Runtime.PYTHON_3_9,
+            code=lambda_.Code.from_asset("src"),
+            handler="new_user.handler",
+            timeout=Duration.seconds(300),
+            environment={"hosted_zone_id": self.hosted_zone.hosted_zone_id},
+            layers=[],
+        )
+        self.POST_signup.queue.grant_consume_messages(self.new_user_lambda)
+        self.POST_signup.queue.grant_send_messages(self.new_user_lambda)
+        self.new_user_lambda.add_event_source(
+            event_sources.SqsEventSource(self.POST_signup.queue, batch_size=1)
+        )
+        permissions = [
+            _ACM_FULL_PERMISSION_POLICY,
+            _SQS_FULL_PERMISSION_POLICY,
+            _ROUTE_53_FULL_PERMISSION_POLICY,
+            _APIGW_FULL_PERMISSION_POLICY,
+        ]
+        for permission in permissions:
+            self.new_user_lambda.role.add_managed_policy(
+                iam.ManagedPolicy.from_aws_managed_policy_name(permission)
+            )
+        self.apis.grant_read_write_data(self.new_user_lambda)
+        self.new_user_lambda.add_environment(self.apis.table_name, self.apis.table_arn)
+
+    def create_cert_for_domain(self) -> acm.Certificate:
+        return acm.Certificate(
+            self,
+            "Certificate",
+            domain_name=f"*.{self.domain_name}",
+            validation=acm.CertificateValidation.from_dns(self.hosted_zone),
+        )
 
     def create_lambda(
         self,
@@ -347,5 +336,43 @@ class NdMainStack(Stack):
 
         return LambdaQueueTuple(_lambda, _queue)
 
-    def import_dynamodb_table(self, name: str) -> dynamodb.ITable:
-        return dynamodb.Table.from_table_name(self, name, f"{self.prefix}_{name}")
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        prefix: str,
+        domain_name: str,
+        region_name: str,
+        **kwargs,
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.prefix = prefix
+        self.region_name = region_name
+        self.domain_name = domain_name
+
+        # Imports
+        self.import_secrets()
+        self.import_lambda_layers()
+        self.import_databases()
+        self.import_buckets()
+
+        # DNS
+        self.hosted_zone = self.import_hosted_zone()
+        self.main_cert = self.create_cert_for_domain()
+
+        # API Gateway and lambda-integrated routes
+        self.create_api_gateway_and_lambdas()
+
+        # Add record to route53 pointing "api" subdomain to api gateway
+        api_record = route53.ARecord(
+            self,
+            "ApiRecord",
+            record_name=f"api.{self.domain_name}",
+            zone=self.hosted_zone,
+            target=route53.RecordTarget.from_alias(targets.ApiGateway(self.api)),
+        )
+
+        # Additional lambdas
+        self.create_new_user_lambda()
+        self.create_delete_user_lambda()
