e5fcc6d59eb615a6a88b17a5ba88c7e0a94ebcf6
Vincent Chan <vincent@doingcloudstuff.com>
2023-03-16 23:40:11 -0700

    add GET /ml-models/{model_name} operation

--- src/ml_models_GET.py
+++ src/ml_models_GET.py
@@ -1,12 +1,37 @@
+import os
 import json
+from datetime import datetime
 from helpers import validation
+import boto3
+
+_REGION_NAME = os.environ["region_name"]
+MODELS_S3_BUCKET = f"neurodeploy-models-{_REGION_NAME}"
+
+s3 = boto3.client("s3")
+
+
+def get_model_info(username: str, model_name: str) -> dict:
+    key = f"{username}/{model_name}"
+    res = s3.head_object(Bucket=MODELS_S3_BUCKET, Key=key)
+    updated_at: datetime = res["LastModified"]
+    return {
+        "model_name": model_name,
+        "model_type": res["Metadata"]["model-type"],
+        "persistence_type": res["ContentType"].split("/")[1],
+        "uploaded_at": updated_at.isoformat(),
+    }
 
 
 @validation.check_authorization
 def handler(event: dict, context):
+    username = event["username"]
+    path_params = event["path_params"]
+    model_name = path_params["proxy"]
     return {
         "isBase64Encoded": False,
-        "statusCode": 201,
-        "headers": {"headerName": "headerValue"},
-        "body": "...",
+        "statusCode": 200,
+        "body": json.dumps(
+            get_model_info(username=username, model_name=model_name),
+            default=str,
+        ),
     }
