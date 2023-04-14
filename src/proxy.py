c6087e2382c265552b0d89edb3fbec2c29208883
Vincent Chan <vincent@doingcloudstuff.com>
2023-04-14 15:54:04 -0700

    allow proxy with GET

--- src/proxy.py
+++ src/proxy.py
@@ -156,7 +156,7 @@ def handler(event: dict, context) -> dict:
         "headers": {
             "Access-Control-Allow-Origin": "*",  # Required for CORS support to work
             "Access-Control-Allow-Credentials": True,  # Required for cookies, authorization headers with HTTPS
-            "Access-Control-Allow-Methods": "POST",  # Allow only POST request
+            "Access-Control-Allow-Methods": "*",
             "Access-Control-Allow-Headers": "Content-Type",
         },
         "body": json.dumps({"output": result["output"]}, default=str),
